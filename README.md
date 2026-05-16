7a0a55f93a0bdbd871eeb14556c162279c37f2ee
Subahan Ali <subahanislam523@gmail.com>
2026-05-16 12:22:03 -0600

    adde to data create findmany findunique findfirstorthrow error

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 2nd . migrate - done
 3rd . jenarate - done
 4th . queary -pending
+
+##
+- create
+- findMany - array and object
+- findUnique - one sigle data dicce
+- findFirstOrThrow - return single object if exits , other wise throw error
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,30 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 async function main() {
   try {
-    const result = await prisma.user.create({
-      data: {
-        name: "subahan",
-        email: "subahanislam523@gmail.com",
-        profilePhoto:"https://ibb.co.com/xK4gS5s4"
-      },
+    // const result = await prisma.user.create({
+    //   data: {
+    //     name: "subahan",
+    //     email: "subahanislam523@gmail.com",
+    //     profilePhoto:"https://ibb.co.com/xK4gS5s4"
+    //   },
+    // });
+    // const userData = await prisma.user.findMany({
+    //   where : {
+    //    name:"subahan"
+    //   }
+    // });
+    // console.log(" User data:", userData);
+
+    // const findUserById = await prisma.user.findUnique({
+    //   where :{
+    //     id:1
+    //   }
+    // });
+    const findUserById = await prisma.user.findFirstOrThrow({
+      where :{
+        id:7 }
     });
-    console.log(" User created successfully:", result);
+    console.log(" User data find By id:", findUserById);
   } catch (error) {
     console.error(" Error details:", error);
   } finally {
