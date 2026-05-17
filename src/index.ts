0c23af88246b9963a39845d1cee13679a7833f83
Subahan Ali <subahanislam523@gmail.com>
2026-05-17 11:40:58 -0600

    added to all find data and update data

--- src/index.ts
+++ src/index.ts
@@ -23,11 +23,27 @@ async function main() {
     //     id:1
     //   }
     // });
-    const findUserById = await prisma.user.findFirstOrThrow({
-      where :{
-        id:7 }
-    });
-    console.log(" User data find By id:", findUserById);
+
+    // const findUserById = await prisma.user.findFirstOrThrow({
+    //   where :{
+    //     id:7 }
+    // });
+    // console.log(" User data find By id:", findUserById);
+
+
+    // update user data
+    // const updateUserData = await prisma.user.update({
+    //   where: {
+    //     id: 1
+    //   },
+    //   data: {
+    //     name: "subahan islam"
+    //   }
+    // })
+    // console.log(" Updated user data:", updateUserData);
+    const userallData = await prisma.user.findMany();
+    console.log(" All user data:", userallData);
+
   } catch (error) {
     console.error(" Error details:", error);
   } finally {
